2141179404cd406b5e368580f89d4f493e840e41
John Singapuri <john.singapuri@gmail.com>
2021-10-29 19:49:29 -0600

    Final

--- routes/view.js
+++ routes/view.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const path = require("path");
 
-router.post("/exercise", (req, res) => {
+router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
-router.post("/stats", (req, res) => {
+router.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
