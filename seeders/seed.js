ac92f0692652e76365908fd3d0a87e8d2feffc33
John Singapuri <john.singapuri@gmail.com>
2021-10-29 19:35:21 -0600

    done

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 const db = require('../models');
 
-mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/SingapuriFit', 
-{
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
-  useCreateIndex: true
 });
 
 const workoutSeed = [
